7ba76bfb2aadb8ecd5b892a95ba59ddcc64f284a
jcardillo123 <jacob.cardillo@kearsarge.org>
2018-12-18 08:22:15 -0500

    needed to pull

---
+++ Scoreboard.py
@@ -0,0 +1,17 @@
+import pygame, sys, math
+import random
+import time
+
+from getpos import *
+from sprite_class import *
+from lifeboard import
+
+pygame.init()
+
+width= 1600
+height= 900
+size = width, height
+
+
+def score_board(screen, p1s, p2s)
+
