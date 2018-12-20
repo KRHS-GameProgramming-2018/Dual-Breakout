d996bbe42ac52fd0fbb89efd0c88bb795b0c6d23
Lucas Carey <lucas.carey@kearsarge.org>
2018-12-20 07:54:26 -0500

    updated

--- Block.py
+++ Block.py
@@ -2,7 +2,7 @@ import pygame, sys, math
 
 class Block():
     def __init__(self,  pos=[0,0]):
-        self.images = [pygame.image.load("Blocks/wall11.png"),
+        self.image = pygame.image.load("Blocks/wall1.png")
                        
         self.rect = self.image.get_rect(center=pos)
         self.radius = (self.rect.width/2 + self.rect.height/2)/2
