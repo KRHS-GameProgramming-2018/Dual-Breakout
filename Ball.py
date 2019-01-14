c94ea09f7408f41fd781d176f53c036788683420
yaboitucktruck <tucker.cushing@kearsarge.org>
2019-01-14 07:45:32 -0500

    changes to ball

--- Ball.py
+++ Ball.py
@@ -154,5 +154,5 @@ class Ball():
                                             self.didBounceY = True
 
                                     return True
-        return False
+            return False
     
