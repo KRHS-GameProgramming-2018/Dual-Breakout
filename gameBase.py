dc9a6cf9ea4a2ae2076b75f668d35815be1fc0cd
yaboitucktruck <tucker.cushing@kearsarge.org>
2019-02-20 08:01:42 -0500

    fixed bad merge

--- gameBase.py
+++ gameBase.py
@@ -209,13 +209,13 @@ while True:
             if not block.living:
                 level.remove(block)
                 
-<<<<<<< HEAD
+
         #if len(blocks) < abs(rkt.score - rkt2.score):
             #mode = "end"
-=======
+
         if len(level) < abs(rkt.score - rkt2.score):
             mode = "end"
->>>>>>> origin/master
+
     
         screen.blit(bgimage, bgrect)
         if dbgTime: print "\t\t Time background  Draw:", time.clock() - start  
